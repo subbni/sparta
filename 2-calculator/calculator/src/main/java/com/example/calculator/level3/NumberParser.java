228d330f12a1c2b33df1694a5449d0fea1d556ea
subbni <slfkak@naver.com>
2025-01-08 15:33:36 +0900

    refactor: NumberParser 메서드에 throws로 런타임 예외 명시 추가 - 예외 처리 시 예상 가능한 예외를 더 쉽게 파악 가능

--- 2-calculator/calculator/src/main/java/com/example/calculator/level3/NumberParser.java
+++ 2-calculator/calculator/src/main/java/com/example/calculator/level3/NumberParser.java
@@ -4,7 +4,8 @@ public class NumberParser {
     /**
      * 소수점 여부에 따라 Double 또는 Integer를 반환합니다.
      */
-    public static Number parse(String number) {
+    public static Number parse(String number) throws NumberFormatException { // 예외 처리를 강제하지는 않지만, 해당 예외의 발생 가능성을 알림
+
         if(number.contains(".")) {
             return Double.parseDouble(number);
         } else {
