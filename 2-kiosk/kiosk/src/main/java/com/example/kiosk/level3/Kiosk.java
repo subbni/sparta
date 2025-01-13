a46cb43d45ca8ac704ee8d54d1ee7d9c80848323
subbni <slfkak@naver.com>
2025-01-13 20:57:10 +0900

    fix: 출력문구 오타 수정 (#10)

--- 2-kiosk/kiosk/src/main/java/com/example/kiosk/level3/Kiosk.java
+++ 2-kiosk/kiosk/src/main/java/com/example/kiosk/level3/Kiosk.java
@@ -33,7 +33,7 @@ public class Kiosk {
                 } else if(inputIdx>0 && inputIdx<=menuItems.size()){ // 1 ~ menuItems.size() : 해당 메뉴 선택 처리
                     System.out.printf("선택한 메뉴 : %s\n", menuItems.get(inputIdx-1));
                 } else { // 그 외 : 잘못된 인덱스 오류 처리
-                    System.out.println("처리에 실패햇습니다. 존재하지 않는 메뉴 번호입니다.");
+                    System.out.println("처리에 실패했습니다. 존재하지 않는 메뉴 번호입니다.");
                 }
             } catch (NumberFormatException e) { // 숫자 형식이 아닌 경우
                 System.out.println("처리에 실패했습니다. 원하는 메뉴 번호를 숫자 형식으로 입력해주세요.");
