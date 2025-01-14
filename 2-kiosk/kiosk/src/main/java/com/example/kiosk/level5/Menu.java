919c8ebd1c1d05c315319f233719206a44612640
subbni <slfkak@naver.com>
2025-01-14 13:44:29 +0900

    feat: 키오스크 Lv.5 구현 (#10)

---
+++ 2-kiosk/kiosk/src/main/java/com/example/kiosk/level5/Menu.java
@@ -0,0 +1,29 @@
+package com.example.kiosk.level5;
+
+import java.util.List;
+
+public class Menu {
+    private final String category;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String category, List<MenuItem> menuItems) {
+        this.category = category;
+        this.menuItems = menuItems;
+    }
+
+    // List에 들어있는 MenuItem을 순차적으로 보여주는 함수
+    public void printMenuItems() {
+        for(int i=0; i<menuItems.size(); i++) {
+            System.out.print(i+1 + ". ");
+            System.out.println(menuItems.get(i));
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
