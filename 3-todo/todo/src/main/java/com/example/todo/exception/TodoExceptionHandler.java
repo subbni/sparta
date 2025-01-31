c98d137c148f139fbc143646b81499335c977998
subbni <slfkak@naver.com>
2025-01-31 16:30:41 +0900

    feat: Request Validation 관련 Custom Exception 추가 및 예외 핸들러 메서드 추가 (#15)

--- 3-todo/todo/src/main/java/com/example/todo/exception/TodoExceptionHandler.java
+++ 3-todo/todo/src/main/java/com/example/todo/exception/TodoExceptionHandler.java
@@ -1,24 +1,49 @@
 package com.example.todo.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
 @Slf4j
 @ControllerAdvice
 public class TodoExceptionHandler {
     @ExceptionHandler(TodoException.class)
     public ResponseEntity<ErrorResponse> handleTodoException(TodoException e) {
-        log.debug("Todo Custom Exception [statusCode = {}, errorMessage = {}, cause = {}]", e.getHttpStatus(), e.getMessage(), e.getStackTrace());
+        log.debug("Todo Custom Exception [statusCode = {}, errorMessage = {}, cause = {}]",
+                e.getHttpStatus(), e.getMessage(), e.getStackTrace());
         return ResponseEntity.status(e.getHttpStatus())
                 .body(ErrorResponse.from(e));
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class) /* @Valid 검증 오류 */
+    public ResponseEntity<ErrorResponse> handleRequestValidationException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request
+    ) {
+        ExceptionType exceptionType = ExceptionType.REQUEST_VALIDATION_FAILED;
+
+        List<String> errors = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> "["+error.getField()+"] " + error.getDefaultMessage())
+                .toList();
+
+        log.debug("Validation Exception [uri = { }, errorMessage = {}, cause = {}]",
+                request.getRequestURI(), e.getMessage(), e.getCause());
+        return ResponseEntity.status(exceptionType.getHttpStatus())
+                .body(new ErrorResponse(exceptionType, String.join(", ",errors)));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("Server exception [errorMessage = {}, cause = {},error ={}]", e.getMessage(), e.getCause(), e.getStackTrace());
+        log.error("Server exception [errorMessage = {}, cause = {}]",
+                e.getMessage(), e.getCause());
         return ResponseEntity.internalServerError()
-                .body(new ErrorResponse(ExceptionType.INTERNAL_SERVER_ERROR, ExceptionType.INTERNAL_SERVER_ERROR.getMessage()));
+                .body(new ErrorResponse(ExceptionType.INTERNAL_SERVER_ERROR,
+                        ExceptionType.INTERNAL_SERVER_ERROR.getMessage()));
     }
 
 }
