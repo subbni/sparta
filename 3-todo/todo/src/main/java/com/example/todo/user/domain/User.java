ff0b7090c41db7ed2ca460cb32b8fae09d5e09c9
subbni <slfkak@naver.com>
2025-02-03 16:17:59 +0900

    fix: user 도메인 필드 빠트린 접근제어자 설정 (#15)

--- 3-todo/todo/src/main/java/com/example/todo/user/domain/User.java
+++ 3-todo/todo/src/main/java/com/example/todo/user/domain/User.java
@@ -13,6 +13,6 @@ public class User {
     private Long id;
     private String name;
     private String email;
-    LocalDateTime createdAt;
-    LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
