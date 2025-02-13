e3f2597d0acf8bbdc15fa4dcf98ff3ba1a2be550
subbni <slfkak@naver.com>
2025-02-13 10:32:27 +0900

    update: Todo에 Comment 관련 로직 추가 (#22)

---
+++ 4-todo-jpa/todo/src/main/java/com/example/todo/todo/service/TodoFacadeService.java
@@ -0,0 +1,56 @@
+package com.example.todo.todo.service;
+
+import com.example.todo.comment.service.CommentService;
+import com.example.todo.todo.dto.CreateTodoRequest;
+import com.example.todo.todo.dto.TodoResponse;
+import com.example.todo.todo.dto.UpdateTodoRequest;
+import com.example.todo.user.domain.User;
+import com.example.todo.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TodoFacadeService {
+
+    private final TodoService todoService;
+    private final UserService userService;
+    private final CommentService commentService;
+
+    @Transactional
+    public TodoResponse create(Long userId, CreateTodoRequest request) {
+        User user = userService.getUserById(userId);
+        return todoService.create(user, request);
+    }
+
+    @Transactional(readOnly = true)
+    public TodoResponse getTodo(Long todoId) {
+        return todoService.getTodo(todoId);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<TodoResponse> getTodos(Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                Sort.by("updatedAt").descending()
+        );
+        return todoService.getTodos(pageRequest);
+    }
+
+    @Transactional
+    public TodoResponse update(Long userId, Long todoId, UpdateTodoRequest request) {
+        return todoService.update(userId,todoId,request);
+    }
+
+    @Transactional
+    public void delete(Long userId, Long todoId) {
+        todoService.delete(userId,todoId);
+        commentService.deleteAllByTodoId(todoId);
+    }
+}
